390ab3d0d58fd2a4e9bf84162be13f35e385ba6a
Vanshika Dawar <vdawar@myseneca.ca>
2025-02-03 17:20:17 -0500

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Author: Vanshika Dawar
+# Author ID: 152288221
+# Date Created: 2004/11/28
+
+# Set the initial value of the timer
+timer = 10
+
+# Loop until timer reaches 0
+while timer > 0:
+    print(timer)
+    timer -= 1  # Decrement the timer by 1
+
+# Print "blast off!" after the loop ends
+print("blast off!")
+
